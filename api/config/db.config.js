5986ae8851d73550aa4e1327db85bd87e07d7c66
nkardj <najib.kardjoudj@epitech.eu>
2021-09-13 17:58:30 +0200

    [WIP] Dockerisation - client OK - API & Mysql OK
    [BUG]
    API not responding, apparently reading from the DB still.

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -2,18 +2,37 @@ const { Sequelize } = require('sequelize');
 
 var host =  process.env.DB_HOST;
 var user =  process.env.DB_USER;
-var userpass =  process.env.DB_USERPASS;
+var userpass =  process.env.DB_PASSWORD;
 var database = process.env.DB_NAME;
+var port = process.env.DB_PORT;
 
 const sequelize =  new Sequelize(database, user, userpass, {
     host: host,
-    dialect: 'mysql'
+    port: port,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 })
 
-try {
-    await sequelize.authenticate();
-} catch (error) {
-    console.error('DB connection error: ', error);
+start().catch((error) => {
+    console.error(error);
+    }
+
+);
+
+async function start() {
+    try {
+        await sequelize.authenticate();
+        console.log("DB connection up.");
+    } catch (error) {
+        console.error('database: ', database, " | user: ", user);
+        console.error('DB connection error: ', error);
+    }
+    await sequelize.sync({alter: true});
 }
 
 module.exports = sequelize;
