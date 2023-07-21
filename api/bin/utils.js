d12019fb8ab00623e2b019b367ae09dbf333783e
najib-k <najib.kardjoudj@gmail.com>
2023-07-21 13:17:47 +0200

    fix: Math.floor => Math.max

--- api/bin/utils.js
+++ api/bin/utils.js
@@ -1,11 +1,11 @@
 /**
  *  
  * @param {number} max Excluded, ie: 3 -> [0,1,2].
- * @param {number} min Added to final result.
+ * @param {number} min Minimum returned value
  * @returns random number.
  */
 function getRandomInt(max, min = 0) {
-    return Math.floor(Math.random() * max) + min;
+    return Math.max(Math.floor(Math.random() * max), min);
 }
 
 module.exports = {
